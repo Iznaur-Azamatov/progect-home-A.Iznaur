e7c6ae1e787ea9a34d9686fc17435cf3457714c6
Azamatov Iznaur <azamatoviznaur@gmail.com>
2023-11-13 11:56:08 +0300

    This is a new commit for what I originally planned to be amended

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let a = '28.02.1999';
+
+/*1-Нужно  использовать тип данных : string*/
+
+let b = 3.14;
+
+/*2-Нужно  использовать тип данных : number*/
+
+const c = 10;
+c + 5;
+console.log(typeof a , typeof b , c)
+/*3-Ответ будет 10 так как переменная const неизменяема*/
+
+let name = prompt('Name?');
+let address = prompt('Address?');
+let phone =prompt('Phone?');
+
+console.log(`Моё имя ${name} , Я живу ${address} , Мой номер ${phone} ` ,alert ('Спасибо!'))
+
+let p = 1 , $ = p*94 , myKapital = p*1000;
+
+   
+ alert(myKapital / $)  
